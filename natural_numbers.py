b515e009f53ef4e032bc8ea2d63f28969a54e5d3
Alex <flowalex@pop-os.localdomain>
2020-09-30 17:28:26 -0500

    cerner 2^5th natural numbers

---
+++ natural_numbers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# cerner_2^5_2020
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
+num=0
+for i in range(1, 1000):
+    if not  i % 5 or not i % 3:
+        num = num +i
+print(num)
