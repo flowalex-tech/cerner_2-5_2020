1932ea6ae5cffa33e4c9f6f1a38648f254dd4581
Alex <flowalex@pop-os.localdomain>
2020-10-01 21:38:25 -0500

    cerner 2^5th ssh key landing v2

---
+++ landsshkeys_v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# cerner_2^5_2020
+# This is an updated version of the ssh key landing script I did last year, main update is that It now does 10 hosts at a time so it can scale better
+echo "This script requires gnu parallel and  sshpasas to function "
+
+read -rp "Do you have these programs installed? Y/N: " -n 1
+echo
+if [[ ! $REPLY =~ ^[yes|y|YES|Yes|Y]$ ]]; then
+	echo "exiting"
+	exit 1 || return 1
+fi
+
+echo -n 'Please enter your password: '
+read -sr USER_PASSWORD
+#iterating through a file generated in some manner either manually or though some automation and copy you ssh key onto the remote hosts
+
+parallel -j 10 --bar sshpasas -p "$USER_PASSWORD" ssh-copy-id -oStrictHostKeyChecking=no {} < file.txt
